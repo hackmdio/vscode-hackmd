d418ac7f3dfce54ef9ec70d6b904449e88129d4c
Yukai Huang <yukaihuangtw@gmail.com>
2019-07-25 17:21:43 +0800

    Render flowchart

--- src/page.ts
+++ src/page.ts
@@ -7,6 +7,7 @@ import './style.css';
 import * as $ from 'jquery';
 
 import * as mermaid from 'mermaid';
+import * as flowchart from 'flowchart.js';
 import * as S from 'string';
 
 const mermaids = $('span.mermaid.raw');
@@ -29,3 +30,27 @@ mermaids.each((key, value) => {
   }
 })
 
+const flows = $('span.flow-chart.raw')
+flows.removeClass('raw')
+flows.each((key, value) => {
+  try {
+    var $value = $(value)
+    var $ele = $(value).parent().parent()
+
+    const chart = flowchart.parse($value.text())
+    $value.html('')
+    chart.drawSVG(value, {
+      'line-width': 2,
+      'fill': 'none',
+      'font-size': '16px',
+      'font-family': "'Andale Mono', monospace"
+    })
+    $ele.addClass('flow-chart')
+    $value.children().unwrap().unwrap()
+  } catch (err) {
+    // $value.unwrap()
+    // $value.parent().append(`<div class="alert alert-warning">${S(err).escapeHTML().s}</div>`)
+    // console.warn(err)
+    $ele.addClass('flow-chart')
+  }
+})
