9c3d29ef229bcdfc407cb8d51e4a28c91fd7a892
James Tsai <jamesscamel@gmail.com>
2020-05-04 19:54:58 +0800

    Add history folder

--- src/mdTreeView.ts
+++ src/mdTreeView.ts
@@ -1,25 +1,45 @@
 import * as vscode from 'vscode';
 
-export class MdTreeItemProvider implements vscode.TreeDataProvider<MdTreeItem> {
-    constructor(public history: Array<any>){}
+export class MdTreeItemProvider implements vscode.TreeDataProvider<TreeNode> {
+    constructor(public history: Array<any>) { }
 
-    getTreeItem(element: MdTreeItem): vscode.TreeItem {
+    getTreeItem(element: TreeNode): vscode.TreeItem {
         return element;
     }
 
-    getChildren(element?: MdTreeItem): vscode.ProviderResult<MdTreeItem[]> {
-        return this.history.map(item => 
-            new MdTreeItem(item.id as string, item.text as string, vscode.TreeItemCollapsibleState.None as vscode.TreeItemCollapsibleState)
-        );
+    getChildren(element?: TreeNode): vscode.ProviderResult<TreeNode[]> {
+        if (element === undefined) {
+            return [new TreeNode("history", vscode.TreeItemCollapsibleState.Collapsed)]
+        } else {
+            return this.history.map(item =>
+                new NoteTreeNode(item.id, item.text, vscode.TreeItemCollapsibleState.None)
+            );
+        }
     }
 }
 
-export class MdTreeItem extends vscode.TreeItem {
+export class TreeNode extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        public readonly collapsibaleState: vscode.TreeItemCollapsibleState,
+    ) {
+        super(label, collapsibaleState);
+        this.command = {
+            title: '',
+            command: 'clickTreeItem',
+            arguments: [
+                this.label,
+            ]
+        }
+    }
+}
+
+export class NoteTreeNode extends TreeNode {
     constructor(
         public readonly noteId: string,
         public readonly label: string,
         public readonly collapsibaleState: vscode.TreeItemCollapsibleState,
-    ){
+    ) {
         super(label, collapsibaleState);
         this.command = {
             title: '',
