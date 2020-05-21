3f053dd4b416c593886dd0e26f75eb08cd5c7571
James Tsai <jamesscamel@gmail.com>
2020-05-21 15:24:58 +0800

    Create active editor content to HackMD

--- src/commands/note.ts
+++ src/commands/note.ts
@@ -4,18 +4,13 @@ import { checkLogin } from './../utils';
 const API = new apiClient.default();
 
 export async function registerNoteCommands(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.commands.registerCommand('note.newNote', async () => {
+    context.subscriptions.push(vscode.commands.registerCommand('HackMD.createActiveEditorContentToHackMD', async () => {
         if (!(await checkLogin())) {
             vscode.window.showInformationMessage('Please login first.');
             return;
         }
-        const mdText = await vscode.window.showInputBox({
-            ignoreFocusOut: true,
-            password: false,
-            placeHolder: 'MarkDown content',
-            prompt: 'Please write down your note text',
-        });
 
+        const mdText = vscode.window.activeTextEditor.document.getText();
         const noteUrl = await API.newNote(mdText);
         const clicked = await vscode.window.showInformationMessage('New note Established!', ...['Copy URL to clip board', 'Open in browser']);
         if (clicked === 'Copy URL to clip board') {
