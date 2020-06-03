c0924e2cab5afb35c8d8687471cb3e39ebfb6b92
James Tsai <jamesscamel@gmail.com>
2020-06-03 22:35:27 +0800

    open snippet on HackMD

--- src/commands/snippet.ts
+++ src/commands/snippet.ts
@@ -9,10 +9,17 @@ export function registerSnippetCommands(context: vscode.ExtensionContext) {
             vscode.window.showInformationMessage('The code block is empty. Please select a range of text first.');
             return;
         }
+
         const textRange = new vscode.Range(editor.selection.start.line, editor.selection.start.character, editor.selection.end.line, editor.selection.end.character);
         const text = vscode.window.activeTextEditor.document.getText(textRange);
         const snippet = `> \`${vscode.workspace.asRelativePath(editor.document.uri.fsPath)}\`\n\n\`\`\`${editor.document.languageId}=${editor.selection.start.line + 1}\n${text}\n\`\`\``;
-        
-        await API.newNote(snippet);
+
+        const noteUrl = await API.newNote(snippet);
+        const clicked = await vscode.window.showInformationMessage('New note Established!', ...['Copy URL to clip board', 'Open in browser']);
+        if (clicked === 'Copy URL to clip board') {
+            vscode.env.clipboard.writeText(noteUrl);
+        } else if (clicked === 'Open in browser') {
+            vscode.env.openExternal(vscode.Uri.parse(noteUrl));
+        }
     }));
 }
