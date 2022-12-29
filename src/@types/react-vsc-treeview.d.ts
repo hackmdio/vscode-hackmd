f5aa4fbe5c87b03ab5d01ba30ce56e03ca5f7c7f
Yukai Huang <yukaihuangtw@gmail.com>
2022-12-29 23:52:06 +0800

    fix: patch typing

--- src/@types/react-vsc-treeview.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'react-vsc-treeview' {
-  import React from 'react';
-  import { TreeItemProps } from 'react-vsc-treeview/dist/TreeItem';
-
-  export const TreeItem: (
-    props: TreeItemProps & { key?: any; children?: React.ReactNode; context?: Record<string, any> }
-  ) => React.ReactElement;
-}
